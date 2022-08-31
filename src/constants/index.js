e24a0b27c52174e602fc19b9780f279361bbe8df
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2022-09-01 05:06:30 +0700

    install jotai and zustand

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export * from "./keys";
 export * from "./paths";
+export * from "./apis";
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import react from "@vitejs/plugin-react";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,7 +9,7 @@ export default defineConfig({
     react({
       jsxImportSource: "@emotion/react",
       babel: {
-        plugins: ["@emotion/babel-plugin"],
+        plugins: ["@emotion/babel-plugin", jotaiDebugLabel, jotaiReactRefresh],
       },
     }),
   ],
