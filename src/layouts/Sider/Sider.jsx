d0fbccfc3bc4a38ca5fbc21ab12bfdcdc05b0b01
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2022-09-06 07:49:28 +0700

    update active menu key

--- src/layouts/Sider/Sider.jsx
+++ src/layouts/Sider/Sider.jsx
@@ -1,15 +1,50 @@
 import { css } from "@emotion/react";
 import { Layout, Menu } from "antd";
+import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { menuItems } from "./menuItems";
 
 const Sider = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const [activeMenuKey, setActiveMenuKey] = useState("");
+
+  const loopMenuArray = useCallback((menuArray = [], pathname = "", activeItem = "") => {
+    for (let i = 0; i < menuArray.length; i++) {
+      const menuItem = menuArray[i];
+
+      const isParent = !!menuItem.children && Array.isArray(menuItem.children) && menuItem.children.length > 0;
+      const isMatchPathname = menuItem.key === pathname;
+
+      if (isParent) {
+        loopMenuArray(menuItem.children, pathname, activeItem);
+      } else if (isMatchPathname) {
+        activeItem.key = menuItem.key;
+        break;
+      }
+    }
+  }, []);
+
+  const getActiveKey = useCallback(() => {
+    let activeItem = {};
+
+    loopMenuArray(menuItems, pathname, activeItem);
+
+    return activeItem?.key || "";
+  }, [loopMenuArray, pathname]);
+
+  useEffect(() => {
+    if (!activeMenuKey) {
+      const key = getActiveKey();
+      setActiveMenuKey(key);
+    }
+  }, [activeMenuKey, getActiveKey]);
 
   const handleChangeMenu = ({ key }) => {
+    setActiveMenuKey(key);
     navigate(key);
   };
 
@@ -21,6 +56,7 @@ const Sider = () => {
         theme="dark"
         items={menuItems.map((item) => ({ ...item, label: t(item.label) }))}
         onSelect={handleChangeMenu}
+        selectedKeys={[activeMenuKey]}
       />
     </Layout.Sider>
   );
